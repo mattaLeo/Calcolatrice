dd70d418bffe66ee9cea5d9371a59b87abe5c5dc
net <net@INF-002.igalvani.local>
2024-03-04 13:31:52 +0100

    v 0.1.0b

--- Calcolatrice/script.js
+++ Calcolatrice/script.js
@@ -1,1 +1,10 @@
-console.log("ciao");
+const txtOperation = document.getElementById("operation");
+
+function writeValue(){
+	txtOperation.innerText + this.value;
+}
+
+function calculate(){
+	let risultato = eval(txtOperation.innerText);
+	txtOperation.innerText = risultato;
+}
